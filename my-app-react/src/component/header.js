663391e0a4f790eed14c302228d83f3015d1692e
lamtaoui mohamed <el-hafid-@hotmail.fr>
2023-11-08 13:28:21 +0100

    links for sign in and return main page was change

--- my-app-react/src/component/header.js
+++ my-app-react/src/component/header.js
@@ -1,23 +1,22 @@
 import Logobank from "../img/argentBankLogo.png"
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
       <div>
         <header >
           <nav class="main-nav">
-            <a class="main-nav-logo" href="./index.html">
+            <Link  class="main-nav-item" to="/">
               <img
                 class="main-nav-logo-image"
                 src={Logobank}
                 alt="Argent Bank Logo"
               />
               <h1 class="sr-only">Argent Bank</h1>
-            </a>
+            </Link>
             <div>
-              <a class="main-nav-item" href="./sign-in.html">
-                <i class="fa fa-user-circle"></i>
-                Sign In
-              </a>
+              <i class="fa fa-user-circle"></i>
+              <Link  class="main-nav-item" to="/signin">Sign In</Link>
             </div>
           </nav>
         </header>
--- my-app-react/src/index.js
+++ my-app-react/src/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
-import Survey from './pages/survey'
+import Signin from './pages/signin'
  
 ReactDOM.render(
     <React.StrictMode>
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/survey" element={<Survey />} />
+                <Route path="/signin" element={<Signin />} />
             </Routes>
-                  </Router>
+        </Router>
     </React.StrictMode>,
 document.getElementById('root')
 )
