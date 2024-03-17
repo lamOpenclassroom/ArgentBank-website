452dc8f591b43a31962b6f8a2523ac839faa49fe
lamtaoui mohamed <el-hafid-@hotmail.fr>
2024-03-17 12:00:06 +0100

    add error component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Signin from './pages/signin'
 import User from './pages/user'
+import Error from './component/error/error'
 import { Provider } from 'react-redux'
 import {store} from "./store"
 
@@ -16,6 +17,7 @@ ReactDOM.render(
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Signin />} />
                     <Route path="/profil" element={<User />} />
+                    <Route path="/*" element={<Error /> } />
                 </Routes>
             </Router>
         </React.StrictMode>
